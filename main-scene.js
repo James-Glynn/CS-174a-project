15005da270e1360ca3a026227814bc72f331eeec
Eugene Choi <choi.eugene.97@gmail.com>
2021-03-19 15:41:35 -0700

    Made the files work

--- main-scene.js
+++ main-scene.js
@@ -46,11 +46,7 @@ Object.assign( defs,
 
 // (Can define Main_Scene's class here)
 
-<<<<<<< HEAD
-const Main_Scene = Collision_Demo;
-=======
 const Main_Scene = Project;
->>>>>>> movement
 const Additional_Scenes = [];
 
 export { Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs }
